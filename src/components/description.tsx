717ceb834a0f77f352992cfcd3a2248dc238a99b
khojwar <khojwartikaram@gmail.com>
2025-06-28 13:56:19 +0545

    destructure props

--- src/components/description.tsx
+++ src/components/description.tsx
@@ -1,13 +1,14 @@
 
 interface IDescriptionText {
     data: string;
+    className?: string;
 }
 
 
-const DescriptionText = (props: Readonly<IDescriptionText>) => {
+const DescriptionText = ({data, className}: Readonly<IDescriptionText>) => {
     return (
-        <div>
-            <p>{props.data}</p>
+        <div className={`text-justify ${className}`}>
+            <p>{data}</p>
         </div>
     )
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,8 +3,8 @@ import DescriptionText from "../components/description";
 const HomePage = () => {
     return (
         <div className="flex flex-col items-center justify-center w-screen h-screen gap-4" >  
-            <DescriptionText data="Welcome to the E-commerce CMS" />
-            <DescriptionText data="This is a simple CMS for managing e-commerce products." />
+            <DescriptionText data="Welcome to the E-commerce CMS" className="text-xl font-bold text-emerald-900" />
+            <DescriptionText data="This is a simple CMS for managing e-commerce products." className="text-md" />
         </div>
     )
 }
